e500d7e020b8905c71635f60fe45f95602372022
Mikhail Natalevich <natalevichmv@gmail.com>
2021-03-12 08:33:11 +0300

    added Complex

---
+++ complex.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+
+class Complex {
+ private:
+  double real_;
+  double imag_;
+  
+ public:
+  // 3 балла
+  Complex();
+  Complex(double real, double imag);
+  Complex(const Complex& other);
+  ~Complex();
+
+  Complex operator=(const Complex& other);
+
+  // 3 балла
+  Complex& operator+=(const Complex& other);
+  Complex& operator*=(const Complex& other);
+
+  friend Complex operator+(const Complex& left, const Complex& right);
+  friend Complex operator*(const Complex& left, const Complex& right);
+
+  friend bool operator==(const Complex& left, const Complex& right);
+  friend bool operator!=(const Complex& left, const Complex& right);
+
+  friend ostream& operator<<(ostream& out, const Complex& complex);
+
+  // 2 балла
+  double Real() const;
+  double Imaginary() const;
+  double Abs() const;
+};
+
+Complex Polar(double radius, double angle);
